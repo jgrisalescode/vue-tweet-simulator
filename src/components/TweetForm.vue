bee41b2780d36843f988a02c7be5469c33a61ab6
Julian Grisales <jgrisales@gmail.com>
2021-03-02 14:54:37 +0100

    Recovery data from form

--- src/components/TweetForm.vue
+++ src/components/TweetForm.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="tweet-form container" :class="{ open: showForm }">
-    <form>
-      <input class="form-control" type="text" placeholder="Tu nombre" />
-      <textarea class="form-control" rows="3" placeholder="Escribe tu tweet"></textarea>
-      <button class="btn btn-primary" type="button">Enviar tweet</button>
+    <form @submit.prevent="sendTweet">
+      <input class="form-control" type="text" placeholder="Tu nombre" v-model="userName"/>
+      <textarea
+          class="form-control"
+          rows="3"
+          placeholder="Escribe tu tweet"
+          v-model="tweet">
+      </textarea>
+      <button class="btn btn-primary" type="submit">Enviar tweet</button>
     </form>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue';
+
 export default {
   props: {
     showForm: {
       type: Boolean
+    }
+  },
+
+  setup() {
+    let userName = ref('');
+    let tweet = ref('');
+    const sendTweet = () => {
+      console.log('Enviando formulario...')
+      console.log('User name -> ' + userName.value)
+      console.log('Tweet -> ' + tweet.value)
+    }
+
+    return {
+      sendTweet,
+      userName,
+      tweet
     }
   }
 }
