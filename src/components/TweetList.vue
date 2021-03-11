ea1229d9ca41e504a306fe43239732fec19bd25e
Julian Grisales <jgrisales@gmail.com>
2021-03-11 15:23:10 +0100

    TweetList template

---
+++ src/components/TweetList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Lista de tweets</h1>
+    <div class="tweet mb-4">
+      <p class="tweet__title">UserName</p>
+      <p class="tweet__text">Tweet</p>
+      <span>01/01/2020</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tweet {
+    position: relative;
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    p {
+      margin: 0;
+    }
+
+    &__title {
+      position: absolute;
+      top: -12px;
+      left: 10px;
+      background-color: white;
+      padding: 0px 10px;
+      font-weight: bold;
+    }
+
+    &__text {
+      color: gray;
+    }
+
+    span {
+      position: absolute;
+      right: 10px;
+      bottom: -9px;
+      border: 1px solid #ccc;
+      font-size: 12px;
+      color: gray;
+      background-color: white;
+      padding: 0 20px;
+    }
+  }
+</style>
